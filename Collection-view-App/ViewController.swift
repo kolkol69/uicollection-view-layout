3288036259d6e9912433bedfa476fec79b8aebf5
kolkol <kolodiy1103@gmail.com>
2019-09-17 22:43:52 +0200

    Initial Commit

---
+++ Collection-view-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collection-view-App
+//
+//  Created by Maksym Kolodiy on 17/09/2019.
+//  Copyright © 2019 Maksym Kolodiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
